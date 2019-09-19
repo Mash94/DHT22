2f0973d8e1fab41c9e39f9ddfd7cd399c031abdf
Nico Mahnic <nmahnic@pioix.edu.ar>
2019-09-19 20:42:47 -0300

    Cambios, no funciona, detecta siempre 1

--- src/DHT22.c
+++ src/DHT22.c
@@ -1,20 +1,9 @@
 #include "chip.h"
 #define TIC_SISTEMA (100)
 #define DHT22_PORT	(2)
-#define DHT22_PIN	(4)
+#define DHT22_PIN	(3)
 #define LED_PORT	(0)
 #define LED_PIN		(22)
-
-void DemoramS(uint32_t milis){
-	uint32_t time = 0;
-	uint32_t i,j;
-
-	DWT->CYCCNT=0;
-	for(i=0 ; i<milis ; i++){
-		for (j=0 ; j<9088 ; j++);
-	}
-	time = DWT->CYCCNT;
-}
 
 /****************************************************************************************
  * Function Name : void DemorauS(uint32_t);
@@ -36,26 +25,39 @@ void DemorauS(uint32_t micros){
 
 void esperaUno(void)
 {
-	int pin;
+	int pin = 0;
 	do{
 		pin = Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN);
+		DemorauS(1);
 	}while((!pin) && (Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN)));
-
+	pin = Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN);
 }
 
 void esperaCero(void)
 {
-	int pin;
+	int pin = 0;
 	do{
 		pin = Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN);
-	}while(pin && (!Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN)));
-
+		DemorauS(1);
+	}while(pin);
+	pin = Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN);
 }
 
 void TareaSensor(void)
 {
 	static int espera = 100;
 	static int estado = 0;
+
+	int i,j,aux;
+	uint8_t val=0;
+	uint8_t trama[5];
+	uint32_t checksum = 0;
+	uint32_t humedad = 0;
+	uint32_t temperatura = 0;
+	bool muestra[450];
+	uint32_t uno =0;
+	uint32_t cero =0;
+	uint32_t time = 0;
 
 	if(!--espera)
 	{
@@ -70,46 +72,58 @@ void TareaSensor(void)
 	}
 	else
 	{
+		for(i=0;i<5;i++){
+			trama[i]=0;
+		}
+		uno = 0;
+		cero = 0;
+
 		Chip_GPIO_SetPinDIRInput(LPC_GPIO, DHT22_PORT, DHT22_PIN);
 		esperaCero();
-		//esperaUno();
-		//esperaCero();
 
-		int i,j,aux;
-		uint8_t val=0;
-		uint8_t trama[5];
-		for(j=0; j<4 ; j++){
+		DWT->CYCCNT=0;
+		esperaUno();
+		time = DWT->CYCCNT;
+		esperaCero();
+
+		for(j=0; j<5 ; j++){
 			for(i=0;i<8;i++)
 			{
+
 				esperaUno();
 				DemorauS(40);
 
 				if(Chip_GPIO_ReadPortBit(LPC_GPIO, DHT22_PORT, DHT22_PIN))
 				{
 					val = 1;
+					uno++;
 				}else{
 					val = 0;
+					cero++;
 				}
-				trama[i]= trama[i]<<1;
-				trama[i]= trama[i]+val;
+				trama[j] = (trama[j]<<1) & 0xFF;
+				trama[j] = (trama[j]+val) & 0xFF;
 			}
 			aux = 1;
 		}
-		Chip_GPIO_SetPinToggle(LPC_GPIO,LED_PORT, LED_PIN);
 
-		uint8_t checksum = 0;
-		for(j=0 ; j<5 ; j++){
-			checksum += trama[j];
+		aux = 1;
+		Chip_GPIO_SetPinToggle(LPC_GPIO,LED_PORT, LED_PIN);
+		humedad = (((uint32_t)trama[0])<<8) + ((uint32_t)trama[1]);
+		temperatura = (((uint32_t)trama[2])<<8) + ((uint32_t)trama[3]);
+
+		for(j=0 ; j<4 ; j++){
+			checksum += ((trama[j]) & 0xFF);
+			checksum &= 0xFF;
 		}
 		if(checksum != trama[4]){
 			for(i = 0 ; i<5 ; i++){
-				trama[i] = 0x0F;
+				trama[i] = 0xFF;
 			}
 		}else{
 			aux = 1;
 		}
 	}
-
 
 }
 
@@ -131,7 +145,7 @@ int main(void) {
 	Chip_GPIO_SetPinOutHigh(LPC_GPIO, LED_PORT, LED_PIN);
 
 	/*Inicializa Systick*/
-	SysTick_Config(SystemCoreClock/TIC_SISTEMA);
+	SysTick_Config(SystemCoreClock/TIC_SISTEMA); //100Hz Corre a 100MHz y cuenta hasta SystemanCr/TIC TIC es la frec que quiero.
 
 	while(1)
 	{
